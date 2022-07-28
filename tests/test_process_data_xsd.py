ded8985cbd2b941c60103c61588ec39c2746c38c
Max Weiner <max.weiner@imf.tu-freiberg.de>
2022-07-28 12:00:25 +0200

    create XSD for process data

---
+++ tests/test_process_data_xsd.py
@@ -0,0 +1,15 @@
+import xmlschema
+from pathlib import Path
+from pprint import pprint
+
+THIS_DIR = Path(__file__).parent
+
+def test_process_data_xsd():
+    schema = xmlschema.XMLSchema("pyroll/ui/gui/res/process_data.xsd")
+
+    schema.validate(THIS_DIR / "process_data.xml")
+
+    data = schema.decode(THIS_DIR / "process_data.xml")
+
+    print()
+    pprint(data)
