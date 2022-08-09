2a41dcdfb18d498760ce7d9099cb9463316ddfcc
Vuizur <Vuizur@users.noreply.github.com>
2022-08-09 20:20:06 +0200

    First version of changing input profile parameters

--- pyroll/gui/pyrollgui.py
+++ pyroll/gui/pyrollgui.py
@@ -1,8 +1,25 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QHeaderView, QTableWidgetItem, QLabel, QComboBox, QFormLayout
-from PySide6.QtCore import QFile, QSize
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QHeaderView,
+    QTableWidgetItem,
+    QLabel,
+    QComboBox,
+    QFormLayout,
+    QLineEdit,
+)
+from PySide6.QtCore import QFile, QSize, Slot
 from ui_mainwindow import Ui_MainWindow
 
+def clearLayout(layout):
+    if layout is not None:
+        while layout.count():
+            child = layout.takeAt(0)
+            if child.widget() is not None:
+                child.widget().deleteLater()
+            elif child.layout() is not None:
+                clearLayout(child.layout())
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -11,19 +28,36 @@ class MainWindow(QMainWindow):
 
         # Set columns of self.ui.rollPassTable to "gap", "roll_radius", "in_rotation", "velocity", "roll_temperature", "transport_duration", "atmosphere_temperature", "roll_rotation_frequency"
         self.ui.rollPassTable.setColumnCount(8)
-        self.ui.rollPassTable.setHorizontalHeaderLabels(["gap", "roll_radius", "in_rotation", "velocity", "roll_temperature", "transport_duration", "atmosphere_temperature", "roll_rotation_frequency"])
+        self.ui.rollPassTable.setHorizontalHeaderLabels(
+            [
+                "gap",
+                "roll_radius",
+                "in_rotation",
+                "velocity",
+                "roll_temperature",
+                "transport_duration",
+                "atmosphere_temperature",
+                "roll_rotation_frequency",
+            ]
+        )
         # Equally space the columns
-        self.ui.rollPassTable.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.ui.rollPassTable.horizontalHeader().setSectionResizeMode(
+            QHeaderView.Stretch
+        )
 
         # Add a row to self.ui.rollPassTable
         self.ui.rollPassTable.insertRow(0)
         self.createInputProfileGUI()
-        #self.createInputProfileOptions()
-        
+        self.createInputProfileOptions()
+
+        # Connect the signal of self.ui.inputProfileBox to the slot createInputProfileOptions
+        self.ui.inputProfileBox.currentIndexChanged.connect(self.createInputProfileOptions)
+
+    @Slot()
     def createInputProfileGUI(self):
         self.ui.inputProfileGrid.setRowMinimumHeight(3, 100)
         # Add items to inputprofilegrid (Horizontally):
-        # 
+        #
         # Create inputProfileLable
         self.ui.inputProfileLabel = QLabel("Input profile")
         # Label "Input profile"
@@ -42,20 +76,24 @@ class MainWindow(QMainWindow):
         # Create inputItemOptions form layout
         self.ui.inputItemOptions = QFormLayout()
         # Set minimum height for inputItemOptions form layout
-        #self.ui.inputItemOptions.SetMinimumSize(QSize(0, 100))
         # Form layout "Input profile options"
         self.ui.inputProfileGrid.addLayout(self.ui.inputItemOptions, 3, 0)
 
-        
+
 
     def createInputProfileOptions(self):
         """Depending on the selected combo box item, create different input profile options"""
         # get the selected item from self.ui.inputProfileBox
         selectedItem = self.ui.inputProfileBox.currentText()
+        #TODO: Finish
+            
+        # Delete all rows from self.ui.inputItemOptions QFormLayout
+        clearLayout(self.ui.inputItemOptions)
+
         # if the selected item is "Square", add to self.ui.inputItemOptions the labels "Diagonal" and "Corner radius" with corresponding line edits
         if selectedItem == "Square":
-            self.ui.inputItemOptions.addRow("Diagonal", self.ui.diagonalInput)
-            self.ui.inputItemOptions.addRow("Corner radius", self.ui.cornerRadiusInput)
+            self.ui.inputItemOptions.addRow(QLabel("Diagonal"), QLineEdit())
+            self.ui.inputItemOptions.addRow(QLabel("Corner radius"), QLineEdit())
 
 
 if __name__ == "__main__":
