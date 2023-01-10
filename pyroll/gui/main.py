3a77219d03cf0a02a7cd1787f1eafc2257b9effa
Vuizur <Vuizur@users.noreply.github.com>
2023-01-10 10:56:41 +0100

    Change logger again for more info

--- pyroll/gui/main.py
+++ pyroll/gui/main.py
@@ -3,9 +3,9 @@ import logging
 from pyroll.gui.pyrollgui import main
 
 if __name__ == "__main__":
-    # FORMAT = "[%(asctime)s %(filename)s->%(funcName)s():%(lineno)s]%(levelname)s: %(message)s"
+    FORMAT = "[%(asctime)s %(filename)s->%(funcName)s():%(lineno)s]%(levelname)s: %(message)s"
 
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.DEBUG, format=FORMAT)
     logging.getLogger('matplotlib.font_manager').disabled = True
 
     # logging.basicConfig(level=logging.DEBUG, format=FORMAT)
